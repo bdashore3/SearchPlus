14650ee0fff5cef7786a5124093a80af533e580a
kingbri <bdashore3@gmail.com>
2022-03-12 23:31:41 -0500

    Initial Commit

---
+++ SearchPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchPlus
+//
+//  Created by Brian Dashore on 3/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchPlus/SearchPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchPlusApp.swift
+//  SearchPlus
+//
+//  Created by Brian Dashore on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
