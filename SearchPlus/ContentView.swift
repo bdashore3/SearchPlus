91468f0e76c1fd5f2a4caa950db104e5d0d908f6
kingbri <bdashore3@gmail.com>
2022-03-17 11:22:14 -0400

    ContentView: Preserve custom search engine URL
    If a user accidentally clicks off the custom URL option, the data
    is replaced with one the suggested search engine URLs.
    
    To fix this, save the last custom search URL and load that if the
    user selects "Custom" again.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- SearchPlus/ContentView.swift
+++ SearchPlus/ContentView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Shared extension variables
     @AppStorage("extensionDisabled") var extensionDisabled: Bool = false
     @AppStorage("queryEngine") var queryEngine: SearchEngine = .init(name: "", URL: "")
     @AppStorage("systemEngine") var systemEngine: SearchEngine = .init(name: "", URL: "")
+
+    // App only variables
+    @AppStorage("lastCustomUrl") var lastCustomUrl: String = ""
 
     @State var showPickerView = false
 
@@ -19,10 +23,14 @@ struct ContentView: View {
             Form {
                 Section(header: Text("Current query URL"),
                         footer: Text("To set a custom URL, select custom and input the search engine query URL in the textbox. \n\nScheme: https://domain.com/search?q=%s")) {
-                    TextField("https://domain.com/search?q=%s", text: $queryEngine.URL)
-                        .keyboardType(.URL)
-                        .disableAutocorrection(true)
-                        .autocapitalization(.none)
+                    TextField("https://domain.com/search?q=%s", text: $queryEngine.URL, onCommit: {
+                        if queryEngine.name == "Custom" {
+                            lastCustomUrl = queryEngine.URL
+                        }
+                    })
+                    .keyboardType(.URL)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
                 }
                 .disabled(queryEngine.name != "Custom")
 
@@ -47,6 +55,11 @@ struct ContentView: View {
                 Section(header: Text("Suggested search engines"),
                         footer: Text("Select a search engine to redirect to from the list or input a custom query URL.")) {
                     PickerView(items: SuggestedEngines, parentEngine: $queryEngine)
+                        .onChange(of: queryEngine.name) { _ in
+                            if queryEngine.name == "Custom" {
+                                queryEngine.URL = lastCustomUrl
+                            }
+                        }
                 }
 
                 Section(footer: Text("Turning on this switch disables search redirection, effectively disabling the extension.")) {
